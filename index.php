74b9063d47edbe071af71b42680a5be739d47617
Jeffrey Saavedra <jeffreysaa@outlook.com>
2016-07-10 15:08:14 -0500

    Insert to

--- index.php
+++ index.php
@@ -4,5 +4,12 @@
   if($con){
     echo "Successful DB";
   }
+  $sql = "INSERT INTO users (USER, PASS) VALUES ('Jeff', '123456')";
+  if ($con->query($sql) === TRUE) {
+    echo "<br>Successful INSERT INTO";
+  }
+  else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+  }
   $con->close();
 ?>
